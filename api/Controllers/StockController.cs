e190e729648321cc8f0aa3ce522aa3a4c150bac8
low-perry <dariohaxhiraj@hotmail.com>
2024-06-24 13:14:10 +0200

    Create Delete endpoint

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -67,6 +67,20 @@ namespace api.Controllers
             return Ok(stock.toStockDto());
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stock);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
         
     }
 }
